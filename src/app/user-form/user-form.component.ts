bcbc3b9f612b5eed61cdb25c8a1d56f7f1f03263
red5 <red5@luxoft.com>
2017-12-27 10:56:20 +0200

    allow submitting users

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../entity/User";
+import {Http} from "@angular/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-form',
@@ -9,10 +11,18 @@ import {User} from "../entity/User";
 export class UserFormComponent implements OnInit {
 
   user: User = new User();
+  url = "http://localhost:8080/users";
 
-  constructor() { }
+  constructor(private http: Http, private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.http.post(this.url, this.user).subscribe(res => {
+      this.router.navigateByUrl("");
+    });
   }
 
 }
